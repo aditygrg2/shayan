a04791b3e857e87872ddfbde37993cc7f164463f
Aditya Garg <uber6707@gmail.com>
2023-03-16 09:31:07 +0530

    redeploy

--- lib/widgets/home_screen/reusable_card_list.dart
+++ lib/widgets/home_screen/reusable_card_list.dart
@@ -6,19 +6,22 @@ class ImageWithTextCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(20),
-        width: 100,
-        height: 100,
-        child: Stack(
-          children: [
-            Container(
-              child: Image.network('https://www.heysigmund.com/wp-content/uploads/2014/10/Most-Relaxing-Song.jpeg',fit:BoxFit.contain ),
-            ),
-            Container(
-              child: Text(''), //'text displayed on Image'
-            )
-          ],
-        ));
+      padding: EdgeInsets.all(20),
+      width: 100,
+      height: 100,
+      child: Stack(
+        children: [
+          Container(
+            child: Image.network(
+                'https://www.heysigmund.com/wp-content/uploads/2014/10/Most-Relaxing-Song.jpeg',
+                fit: BoxFit.contain),
+          ),
+          Container(
+            child: Text(''), //'text displayed on Image'
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -28,7 +31,11 @@ class ReusableCardList extends StatefulWidget {
 }
 
 class _ReusableCardListState extends State<ReusableCardList> {
-  final Reusables = ['a', 'b','c'];  // fetch data for the list will pass from above widget
+  final Reusables = [
+    'a',
+    'b',
+    'c'
+  ]; // fetch data for the list will pass from above widget
   @override
   Widget build(BuildContext context) {
     return Row(
