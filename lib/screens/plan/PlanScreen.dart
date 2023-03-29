f3debea3aedd204e703577fc8b557a1ea7d81254
parteekcoder <parteekgoyal118@gmail.com>
2023-03-29 11:41:04 +0530

    timeline

--- lib/screens/plan/PlanScreen.dart
+++ lib/screens/plan/PlanScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:night_gschallenge/screens/plan/timeline_card.dart';
+import 'package:night_gschallenge/widgets/UI/elevated_button_without_icon.dart';
 import '../../widgets/UI/home_screen_heading.dart';
-import 'package:provider/provider.dart';
-import '../../providers/watch_provider.dart';
-import 'package:timelines/timelines.dart';
 
 class PlanScreen extends StatefulWidget {
   static const routeName = '/plan';
@@ -12,40 +12,26 @@ class PlanScreen extends StatefulWidget {
 }
 
 class _PlanScreenState extends State<PlanScreen> {
+  bool showPlan = false;
+ 
   List<Map<dynamic, dynamic>> timeline = [
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
-    {
-      "time":"08:00 am",
-      "description":"I will wake up at 8 am"
-    },
+    {"time": "08:00", "task": "I will wake up at 8 am"},
+    {"time": "08:00", "task": "I will wake up at 8 am"},
+    {"time": "08:00", "task": "I will wake up at 8 am"},
+    {"time": "08:00", "task": "I will wake up at 8 am"},
+    {"time": "08:00", "task": "I will wake up at 8 am"},
+    {"time": "11:34", "task": "I will wake up at 8 am"},
+    {"time": "11:40", "task": "I will wake up at 8 am"},
+    {"time": "12:00", "task": "I will wake up at 8 am"},
   ];
+  Duration isActiveCheck(String str){
+    var format = DateFormat("HH:mm");
+    var first= format.parse(str);
+    var now= DateTime.now();
+    var second = format.parse("${now.hour}:${now.minute}");
+    return first.difference(second);
+ 
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -56,26 +42,173 @@ class _PlanScreenState extends State<PlanScreen> {
             text: "My Plan",
           ),
           Container(
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height ,
-            child: Timeline.tileBuilder(
-              physics: NeverScrollableScrollPhysics(),
-              theme: TimelineThemeData(color: Color.fromRGBO(143, 227, 221, 1)),
-              builder: TimelineTileBuilder.fromStyle(
-                  contentsAlign: ContentsAlign.alternating,
-                  contentsBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(padding: EdgeInsets.all(10),width: 100,height: 100,child: Text(timeline[index]['description']),decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10))),
-                    );
-                  },
-                  oppositeContentsBuilder: (context, index) {
-                    return Container(padding: EdgeInsets.all(10),child: Text(timeline[index]['time'],style: TextStyle(color: Colors.white),),decoration: BoxDecoration(color: Color.fromRGBO(250, 195, 68, 1),borderRadius: BorderRadius.lerp(BorderRadius.circular(5), BorderRadius.circular(8), 8)),);
-                  },
-                  itemCount: timeline.length),
+            width: 180,
+            height: 180,
+            child: Image.asset(
+              "assets/my_plan.png",
+              fit: BoxFit.cover,
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: Text(
+              'Our smart mix to shape your plan',
+              style: Theme.of(context).textTheme.headlineLarge,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          !showPlan
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Text(
+                        "we’ll support you on your way to more restful,natural sleep,which will lead you to be more energetic and alert. Follow your dynamic timeline schedule for step-by-step guidance.",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Container(
+                      child: Text(
+                        "We promise to ease the change and support you with all we can",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: GridView(
+                        physics: NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3 / 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10),
+                        shrinkWrap: true,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Icon(
+                                  Icons.timeline_rounded,
+                                  color: Colors.black,
+                                  size: 40,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                child: Text(
+                                  "Personalized dynamic timeline",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Icon(Icons.youtube_searched_for,
+                                    color: Colors.black, size: 40),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                child: Text(
+                                  "enriching sleep education content",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Icon(Icons.chat_rounded,
+                                    color: Colors.black, size: 40),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                child: Text(
+                                  "chat with a sleep trainer",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Icon(Icons.timer_outlined,
+                                    color: Colors.black, size: 40),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                child: Text(
+                                  "rightly timed reminders",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Center(
+                      child: ElevatedButtonWithoutIcon(
+                          onPressedButton: () {
+                            setState(() {
+                              showPlan = !showPlan;
+                            });
+                          },
+                          text: "I'm ready"),
+                    )
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ...(timeline.map((e) {
+                      var difference= isActiveCheck(e['time']);
+                      if(difference.compareTo(Duration.zero)>=0)
+                      return TimelineCard(
+                        duration: difference.toString(),
+                        isActive: difference.compareTo(Duration.zero)==0,
+                        task: e['task'],
+                        time: e['time'],
+                      );
+                      else return null;
+                    }).toList().where((element) => element!=null).toList() as List<Widget>),
+                    Container(padding: EdgeInsets.all(10),child: Text("There's more in the Store",style: Theme.of(context).textTheme.headlineMedium,),),
+                    Container(padding: EdgeInsets.all(10),child: Text("Share your sleep experience, daily habits and overall lifestyle patterns. we’ll match you up with training items which specifically suit your needs",style: Theme.of(context).textTheme.bodySmall,textAlign: TextAlign.center,),),
+                    Center(child: ElevatedButtonWithoutIcon(onPressedButton: (){
+                      setState(() {
+                        showPlan=!showPlan;
+                      });
+                    },text: "get your Plan"),)
+                  ],
+                ),
         ],
       ),
     );
