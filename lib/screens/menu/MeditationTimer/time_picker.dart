cffddc6fbe7d81fc19d4c40551476a21b5c9f556
Aditya Garg <uber6707@gmail.com>
2023-05-25 11:16:12 +0530

    Centering meditation timer

--- lib/screens/menu/MeditationTimer/time_picker.dart
+++ lib/screens/menu/MeditationTimer/time_picker.dart
@@ -12,34 +12,36 @@ class TimePicker extends StatefulWidget {
 class _TimePickerState extends State<TimePicker> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-          decoration: BoxDecoration(
-            color: Theme.of(context).canvasColor,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: const Color.fromRGBO(250, 195, 68, 1),
-            ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 15),
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: const Color.fromRGBO(250, 195, 68, 1),
           ),
-          child: TimePickerSpinner(
-            is24HourMode: true,
-            isShowSeconds: true,
-            time: widget._dateTime,
-            normalTextStyle: TextStyle(fontSize: 24, color: Theme.of(context).disabledColor),
-            highlightedTextStyle: TextStyle(fontSize: 28, color: Theme.of(context).secondaryHeaderColor),
-            spacing: 45,
-            itemHeight: 80,
-            isForce2Digits: true,
-            onTimeChange: (time) {
-              setState(() {
-                widget._dateTime = time;
-                widget.callback(time);
-              });
-            },
+        ),
+        child: TimePickerSpinner(
+          is24HourMode: true,
+          isShowSeconds: true,
+          time: widget._dateTime,
+          normalTextStyle: TextStyle(
+            fontSize: 24,
+            color: Theme.of(context).disabledColor,
           ),
+          highlightedTextStyle: TextStyle(
+              fontSize: 28, color: Theme.of(context).secondaryHeaderColor,),
+          spacing: 44,
+          itemHeight: 80,
+          isForce2Digits: true,
+          onTimeChange: (time) {
+            setState(() {
+              widget._dateTime = time;
+              widget.callback(time);
+            });
+          },
         ),
       ),
     );
